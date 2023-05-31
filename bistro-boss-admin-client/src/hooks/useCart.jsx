29dd1ff1accfde57ff57b69c6251d7854326d858
Iftekher Aziz <azizchy02@gmail.com>
2023-05-31 18:11:49 +0600

    axios interceptor using chat gpt

--- bistro-boss-admin-client/src/hooks/useCart.jsx
+++ bistro-boss-admin-client/src/hooks/useCart.jsx
@@ -1,24 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProviders";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
   const { user } = useContext(AuthContext);
   const token = localStorage.getItem("access-token");
+  const [axiosSecure] = useAxiosSecure();
 
-
-  const { refetch, isLoading, data: cart = [] } = useQuery({
+  const {
+    refetch,
+    isLoading,
+    data: cart = [],
+  } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
+      const res = await axiosSecure(`/carts?email=${user?.email}`);
+      console.log(res);
+      return res.data;
+    },
+    /* queryFn: async () => {
       const res = await fetch(
         `http://localhost:5000/carts?email=${user?.email}`,{
           headers: {
-            authorization: `bearer ${token}`,
+            authorization: `Bearer ${token}`,
           },
         }
       );
       return res.json();
-    },
+    }, */
   });
 
   return [cart, refetch, isLoading];
